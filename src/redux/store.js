e1791e3aec51cc568c15f061c127a2bc7ac0579b
sanderdebr <sander606@gmail.com>
2020-02-03 12:25:12 +0100

    'updated'

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 // Putting function inside array
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
